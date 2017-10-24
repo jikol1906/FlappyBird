15ea09e94783581714cba5375c388603df507191
Boris Grunwald <borisgrunwald@gmail.com>
2017-10-24 10:43:54 +0200

    Initial

---
+++ android/src/com/boris/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.boris.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.boris.flappybird.FlappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBird(), config);
+	}
+}
